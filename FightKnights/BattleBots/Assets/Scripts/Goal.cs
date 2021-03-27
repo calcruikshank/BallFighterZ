08ed2adbc3f12bdf359092bf68650dfda2e56019
calcruikshank <59811572+calcruikshank@users.noreply.github.com>
2021-03-27 16:13:19 -0700

    SoccerScore

--- FightKnights/BattleBots/Assets/Scripts/Goal.cs
+++ FightKnights/BattleBots/Assets/Scripts/Goal.cs
@@ -5,13 +5,41 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     public PlayerController player;
-    [SerializeField] int goalColor = 0; //if goal color is 0 its a neutral goal
+    public SoccerBall soccerBall;
+    [SerializeField] int goalColor = -1; //if goal color is -1 its a neutral goal
+    SoccerGameManager soccerGameManager;
+
+    private void Start()
+    {
+        soccerGameManager = FindObjectOfType<SoccerGameManager>();
+    }
     void OnTriggerEnter(Collider other)
     {
+        soccerBall = other.transform.parent.GetComponent<SoccerBall>();
+        if (soccerBall != null)
+        {
+            if (goalColor == 0 && soccerGameManager != null)
+            {
+                soccerGameManager.AddScoreToBlue();
+
+               
+            }
+            if (goalColor == 1 && soccerGameManager != null)
+            {
+                soccerGameManager.AddScoreToRed();
+
+
+            }
+            soccerBall.LoseStock();
+            return;
+        }
+
         player = other.transform.parent.GetComponent<PlayerController>();
         if (player != null)
         {
             player.LoseStock();
         }
+        
+        
     }
 }
--- FightKnights/BattleBots/Assets/Scripts/SoccerGameManager.cs
+++ FightKnights/BattleBots/Assets/Scripts/SoccerGameManager.cs
@@ -5,19 +5,51 @@ using UnityEngine;
 public class SoccerGameManager : MonoBehaviour
 {
     PlayerTeams playerTeams;
+    SoccerCanvasBehaviour soccerCanvasBehavior;
+    [SerializeField] GameObject soccerCanvas;
+    Canvas canvas;
+
+    public int redScore, blueScore = 0;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+    }
     void Start()
     {
+        canvas = FindObjectOfType<Canvas>();
         playerTeams = FindObjectOfType<PlayerTeams>();
+        
+
         if (!playerTeams.teamsIsOn)
         {
             playerTeams.ToggleTeamsOn();
         }
+
+        GameObject soccerCanvasSpawned = Instantiate(soccerCanvas, Vector3.zero, Quaternion.identity);
+
+        soccerCanvasSpawned.transform.position = Vector3.zero;
+        soccerCanvasSpawned.transform.parent = canvas.transform;
+        soccerCanvasSpawned.transform.localPosition = Vector3.zero;
+        soccerCanvasBehavior = soccerCanvasSpawned.GetComponent<SoccerCanvasBehaviour>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AddScoreToRed()
+    {
+        Debug.Log("AddToRed");
+        redScore++;
+        soccerCanvasBehavior.UpdateText(redScore, blueScore);
+    }
+    public void AddScoreToBlue()
+    {
+        Debug.Log("AddToBlue");
+        blueScore++;
+        soccerCanvasBehavior.UpdateText(redScore, blueScore);
     }
 }
